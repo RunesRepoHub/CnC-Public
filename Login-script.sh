a72cf750bcee4addb4f024dc9c966646f2162def
Rune004 <christian.rune1998@gmail.com>
2023-08-04 23:17:48 +0200

    Added more security and version and scriptname

--- Login-script.sh
+++ Login-script.sh
@@ -1,3 +1,11 @@
+# Set Version 
+version="v0.0.5"
+export version="$version"
+#-------------------------------------------------------------------------------------------------------------------------
+# Set the overall script name
+scriptname="RPH CnC BASH Menu"
+export scriptname="$scriptname"
+#-------------------------------------------------------------------------------------------------------------------------
 # Get the filename for display
 me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 #-------------------------------------------------------------------------------------------------------------------------
@@ -22,6 +30,12 @@ password=$(\
 # Check user and pass against my database
 user_and_pass=$(curl -sS --user  PGSj7EDLrESqn3Rbn:GgJkVVkP8H8TyDmxBiybM3gDzuCpkAuJgrhiFeicwkdvfAmLx9MzYGVWjLDfD "https://n8n-b.rp-helpdesk.com/webhook/login?user=$username&pass=$password")
 #-------------------------------------------------------------------------------------------------------------------------
+# Get the username from the database
+userdb=$(curl -s --user $user_and_pass https://n8n-b.rp-helpdesk.com/webhook/login-user?user=$username)
+#-------------------------------------------------------------------------------------------------------------------------
+# Get the password from the database
+passdb=$(curl -s --user $user_and_pass https://n8n-b.rp-helpdesk.com/webhook/login-pass?pass="$password")
+#-------------------------------------------------------------------------------------------------------------------------
 # Pull down a fresh session ID
 sessionid=$(curl -s --user $user_and_pass https://n8n-b.rp-helpdesk.com/webhook/sessionid)
 #-------------------------------------------------------------------------------------------------------------------------
@@ -37,3 +51,12 @@ export user_and_pass="$user_and_pass"
 export token="$token"
 export sessionid="$sessionid"
 #-------------------------------------------------------------------------------------------------------------------------
+# Check the username and password are valid or not
+if (( $user == "$userdb" && $pass == "$passdb" && $sessionid == "$sessionid" ))
+then
+    clear
+    dialog --title "Login" --backtitle "$scriptname - Version $version" --infobox "Successful login" 10 60 ; sleep 5
+else 
+    clear
+    dialog --title "Login" --backtitle "$scriptname - Version $version" --infobox "Unsuccessful login" 10 60 ; sleep 5
+fi
