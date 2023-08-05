004b5f0a51c3930dcd4bec2532363279649d7646
Rune004 <christian.rune1998@gmail.com>
2023-08-05 22:39:11 +0200

    Added github token function

--- Login-script.sh
+++ Login-script.sh
@@ -119,7 +119,7 @@ then
 
     dialog --title "$me" --clear \
            --backtitle "$scriptname - Version $version" \
-           --prgbox "Git Clone CnC" "git clone https://$token@github.com/rune004/CnC/releases/tag/$version.tar.gz" 30 60 ;
+           --prgbox "Git Clone CnC" "git clone https://rune004:$token@github.com/rune004/CnC/releases/tag/$version.tar.gz" 30 60 ;
 
 else 
     clear
