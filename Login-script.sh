d3deb90a99d56f6f58605c7f282bd08cb07a0f84
Rune004 <christian.rune1998@gmail.com>
2023-08-05 22:54:01 +0200

    Test

--- Login-script.sh
+++ Login-script.sh
@@ -119,7 +119,7 @@ then
 
     dialog --title "$me" --clear \
            --backtitle "$scriptname - Version $version" \
-           --prgbox "Git Clone CnC" "git clone https://rune004:$token@github.com/rune004/CnC/tree/$version.git" 30 60 ;
+           --prgbox "Git Clone CnC" "git clone https://rune004:$token@github.com/rune004/releases/tag/$version.git" 30 60 ;
 
 else 
     clear
