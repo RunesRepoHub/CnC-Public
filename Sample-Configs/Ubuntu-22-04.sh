9235ad89c889386239ee50da872e98066ae2e96a
Rune004 <christian.rune1998@gmail.com>
2023-08-06 18:41:32 +0200

    Added all docker-testing

---
+++ Sample-Configs/Ubuntu-22-04.sh
@@ -0,0 +1,27 @@
+## |--------------------------------|
+## |       Made By Rune004          |
+## |    Need Support or Help?       |
+## |    It can be found below.      |
+## |                                |
+## |            Discord:            |
+## | https://discord.gg/UHd4tJg9Vm  |
+## |                                |
+## |            Github:             |
+## |  https://github.com/rune004    |
+## |--------------------------------|
+SCRIPT="Ubuntu-22-04"
+
+docker stop $SCRIPT > /dev/null 2>&1
+docker rm $SCRIPT > /dev/null 2>&1
+docker volume prune -y > /dev/null 2>&1
+
+docker run -i -d --name="$SCRIPT" ubuntu:22.04 > /dev/null 2>&1
+docker exec -i $SCRIPT /bin/bash > /dev/null 2>&1
+apt-get update > /dev/null 2>&1
+apt-get upgrade -y > /dev/null 2>&1
+apt-get install curl -y > /dev/null 2>&1
+apt-get install git -y > /dev/null 2>&1
+apt-get install sudo -y > /dev/null 2>&1
+apt-get install dialog > /dev/null 2>&1
+cd root > /dev/null 2>&1
+git clone https://github.com/rune004/CnC-Public.git > /dev/null 2>&1
