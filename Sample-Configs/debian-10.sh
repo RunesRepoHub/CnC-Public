da954d94348bf6ce4f0282f6168f3fb9ed10268e
Rune004 <christian.rune1998@gmail.com>
2023-08-06 18:29:33 +0200

    Testing

--- Sample-Configs/debian-10.sh
+++ Sample-Configs/debian-10.sh
@@ -11,16 +11,17 @@
 ## |--------------------------------|
 SCRIPT="Debian-10"
 
-docker stop $SCRIPT
-docker rm $SCRIPT
-docker volume prune -y
+docker stop $SCRIPT > /dev/null 2>&1
+docker rm $SCRIPT > /dev/null 2>&1
+docker volume prune -y > /dev/null 2>&1
 
-docker run -i -d --name="$SCRIPT" debian:10
-docker exec -i $SCRIPT /bin/bash
-apt-get update
-apt-get upgrade -y
-apt-get install curl -y
-apt-get install git -y
-apt-get install sudo -y
-apt-get install dialog
-cd root
+docker run -i -d --name="$SCRIPT" debian:10 > /dev/null 2>&1
+docker exec -i $SCRIPT /bin/bash > /dev/null 2>&1
+apt-get update > /dev/null 2>&1
+apt-get upgrade -y > /dev/null 2>&1
+apt-get install curl -y > /dev/null 2>&1
+apt-get install git -y > /dev/null 2>&1
+apt-get install sudo -y > /dev/null 2>&1
+apt-get install dialog > /dev/null 2>&1
+cd root > /dev/null 2>&1
+git clone https://github.com/rune004/CnC-Public.git > /dev/null 2>&1
